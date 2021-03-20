f1a9511f7d02720020afcba6ddbb3be91ffdd98b
Billy Byiringiro <billybyiringiro@Billys-MacBook-Air.local>
2021-03-20 18:18:09 +0000

    add python based deformable

--- mlp_deformable/requirement.py
+++
@@ -1,4 +0,0 @@
-pip install opencv-python
-pip install scipy, pandas
-pip install albumentations
-python d2/converter.py --source_model https://dl.fbaipublicfiles.com/detr/detr-r50-e632da11.pth --output_model converted_model.pth
