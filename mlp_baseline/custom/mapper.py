e366ba23c7e648df7d2c98f8ab6cc68321d5c580
Billy Byiringiro <billybyiringiro@pc-142-86.customer.ask4.lan>
2021-03-22 23:37:51 +0000

    deploy cutmix fix on mlp_baseline

--- mlp_baseline/custom/mapper.py
+++ mlp_baseline/custom/mapper.py
@@ -97,7 +97,11 @@ def load_cutmix_image_and_boxes(one_dict,img1, all_dataset_dicts,imsize=256):
             padh = y1a - y1b
 
             for j in range(len(img_d['annotations'])):
-                img_d['annotations'][j]['bbox'] = np.clip([img_d['annotations'][j]['bbox'][0]+padw, img_d['annotations'][j]['bbox'][1]+padh, img_d['annotations'][j]['bbox'][2]+padw, img_d['annotations'][j]['bbox'][3]+padh],0, 2 * s)
+                new_bbox = np.clip([img_d['annotations'][j]['bbox'][0]+padw, img_d['annotations'][j]['bbox'][1]+padh, img_d['annotations'][j]['bbox'][2]+padw, img_d['annotations'][j]['bbox'][3]+padh],0, 2 * s)
+                if new_bbox[0] == new_bbox[2] or new_bbox[1] == new_bbox[3]:
+                    del img_d['annotations'][j]
+                    break
+                img_d['annotations'][j]['bbox'] = new_bbox
             img_dict_result['annotations'] +=img_d['annotations']
 
 
